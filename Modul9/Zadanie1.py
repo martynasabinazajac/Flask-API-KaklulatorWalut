2fb731fadacdfda0de87b5074114decf11b7cddd
martynasabinazajac <125702818+martynasabinazajac@users.noreply.github.com>
2023-04-05 11:28:24 +0200

    Add files via upload

--- Modul9/Zadanie1.py
+++ Modul9/Zadanie1.py
@@ -5,37 +5,49 @@ import json
 import requests
 import csv
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-data = response.json()
-print(data)
+# response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+# data = response.json()
 
-# print(data_rates)
 
-with open("plikzdanymi.csv", 'w') as csvfile:
-    fieldnames=['currency', 'code', 'bid', 'ask']
-    writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
-    writer.writeheader()
-    for i in data[0]['rates']:
-        writer.writerow(i)
-        
+#utworzenie pliku csv
+def plikcsv():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    with open("plikzdanymi.csv", 'w') as csvfile:
+        fieldnames=['currency', 'code', 'bid', 'ask']
+        writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+        writer.writeheader()
+        for i in data[0]['rates']:
+            writer.writerow(i)
+
+if __name__ == '__main__':
+    plikcsv()
+
 #kalkulator
 app = Flask(__name__)
+
+def slownik():
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+    data = response.json()
+    rates=data[0]['rates']
+    rates2={i['code']:i['bid'] for i in rates}
+    return rates2
+
 
 
 @app.route("/kalkulator", methods=['GET', 'POST'])
 def calculator():
-    rates=data[0]['rates']
+    rates3=slownik()
     if request.method=='GET':
-        items=[i['code'] for i in rates]
+        items=[keys for keys in rates3]
         return render_template("kalkulator.html", items=items)
     elif request.method == 'POST':
-        waluta=request.form['waluta']
-        kwota=request.form['kwota']
-        for i in rates:
-            waluta== i['code']
-            wynik=float(kwota) * (i['bid'])
-            print(wynik)
-            return f'Wynik: {wynik}'
+        waluta=request.form.get("waluta")
+        kwota=float(request.form['kwota'])
+        waluta2=rates3[waluta]
+        wynik= kwota * waluta2
+        print(wynik)
+        return render_template("kalkulator2.html", wynik=wynik)
 
 
 
