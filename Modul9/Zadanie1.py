ad6f2ab4efcb1cbc821c2cbb5fb9ce77b056c8ad
martynasabinazajac <125702818+martynasabinazajac@users.noreply.github.com>
2023-04-06 21:58:41 +0200

    Add files via upload

--- Modul9/Zadanie1.py
+++ Modul9/Zadanie1.py
@@ -5,14 +5,12 @@ import json
 import requests
 import csv
 
-# response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-# data = response.json()
+response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+data = response.json()
 
 
 #utworzenie pliku csv
 def plikcsv():
-    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-    data = response.json()
     with open("plikzdanymi.csv", 'w') as csvfile:
         fieldnames=['currency', 'code', 'bid', 'ask']
         writer=csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
@@ -20,15 +18,11 @@ def plikcsv():
         for i in data[0]['rates']:
             writer.writerow(i)
 
-if __name__ == '__main__':
-    plikcsv()
 
 #kalkulator
 app = Flask(__name__)
 
 def slownik():
-    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
-    data = response.json()
     rates=data[0]['rates']
     rates2={i['code']:i['bid'] for i in rates}
     return rates2
@@ -39,17 +33,19 @@ def slownik():
 def calculator():
     rates3=slownik()
     if request.method=='GET':
-        items=[keys for keys in rates3]
+        items=rates3.keys()
         return render_template("kalkulator.html", items=items)
     elif request.method == 'POST':
+        items=rates3.keys()
         waluta=request.form.get("waluta")
         kwota=float(request.form['kwota'])
         waluta2=rates3[waluta]
         wynik= kwota * waluta2
         print(wynik)
-        return render_template("kalkulator2.html", wynik=wynik)
+        return render_template("kalkulator.html", wynik=wynik, items=items)
 
 
 
 if __name__ == '__main__':
+    plikcsv()
     app.run(debug=True)
